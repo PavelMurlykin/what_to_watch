be727e73691e6e97745ab9df2e5e07d8815fd470
Pavel Murlykin <pamurlykin@gmail.com>
2026-09-05 13:12:29 +0300

    Добавлены шаблоны

--- opinions_app.py
+++ opinions_app.py
@@ -1,46 +1,37 @@
 from datetime import datetime
-
 from random import randrange
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Подключить БД SQLite.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
-# Создать экземпляр SQLAlchemy и в качестве параметра
-# передать в него экземпляр приложения Flask.
 db = SQLAlchemy(app)
 
 class Opinion(db.Model):
-    # ID — целое число, первичный ключ.
     id = db.Column(db.Integer, primary_key=True)
-    # Название фильма — строка длиной 128 символов, не может быть пустым.
     title = db.Column(db.String(128), nullable=False)
-    # Мнение о фильме — большая строка, не может быть пустым, 
-    # должно быть уникальным.
     text = db.Column(db.Text, unique=True, nullable=False)
-    # Ссылка на сторонний источник — строка длиной 256 символов.
     source = db.Column(db.String(256))
-    # Дата и время — текущее время, 
-    # по этому столбцу база данных будет проиндексирована.
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 @app.route('/')
 def index_view():
-    # Определить количество мнений в базе данных.
     quantity = Opinion.query.count()
-    # Если мнений нет...
     if not quantity:
-        # ...то вернуть сообщение:
         return 'В базе данных мнений о фильмах нет.'
-    # Иначе выбрать случайное число в диапазоне от 0 до quantity...
     offset_value = randrange(quantity)
-    # ...и определить случайный объект.
+    # Извлечь все записи, пропуская первые offset_value записей
+    # и взять первую запись из получившегося набора.
     opinion = Opinion.query.offset(offset_value).first()
-    return opinion.text
+    # Передать в шаблон весь объект opinion.
+    return render_template('index.html', opinion=opinion)
+
+@app.route('/add')
+def add_opinion_view():
+    return 'Страница в разработке!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run() 
