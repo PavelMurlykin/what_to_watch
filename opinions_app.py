995a611a9b5be62fd15f04afb0f6e9dc54c8b67c
Pavel Murlykin <pamurlykin@gmail.com>
2026-09-06 10:59:41 +0300

    Пользовательские команды Flask

--- opinions_app.py
+++ opinions_app.py
@@ -1,3 +1,5 @@
+import csv
+import click
 from datetime import datetime
 from random import randrange
 
@@ -103,6 +105,27 @@ def add_opinion_view():
 def opinion_view(id):
     opinion = Opinion.query.get_or_404(id)
     return render_template('opinion.html', opinion=opinion)
+
+
+@app.cli.command('load_opinions')
+def load_opinions_command():
+    """Функция загрузки мнений в базу данных."""
+    # Открыть файл.
+    with open('opinions.csv', encoding='utf-8') as f:
+        # Создать итерируемый объект, который отображает каждую строку
+        # в качестве словаря с ключами из шапки файла.
+        reader = csv.DictReader(f)
+        # Для подсчёта строк добавить счётчик.
+        counter = 0
+        for row in reader:
+            # Распакованный словарь использовать
+            # для создания экземпляра модели Opinion.
+            opinion = Opinion(**row)
+            # Добавить объект в сессию и закоммитить
+            db.session.add(opinion)
+            db.session.commit()
+            counter += 1
+    click.echo(f'Загружено мнений: {counter}') 
 
 
 if __name__ == '__main__':
